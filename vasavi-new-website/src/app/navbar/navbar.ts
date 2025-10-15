0a031741249524779db0cd216a21a0d3d793491f
nithithehero <mknithish03@gmail.com>
2025-10-15 15:57:15 +0530

    frontend changes

--- vasavi-new-website/src/app/navbar/navbar.ts
+++ vasavi-new-website/src/app/navbar/navbar.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink,RouterModule ],
+  imports: [RouterLink, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  @ViewChild('navbarCollapse') navbarCollapse!: ElementRef;
+
+
+  closeNavbar() {
+    const navbar = this.navbarCollapse?.nativeElement;
+    if (navbar && navbar.classList.contains('show')) {
+      navbar.classList.remove('show');
+    }
+  }
+
+  onNavClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains('dropdown-toggle') || target.closest('.dropdown-toggle')) {
+      return;
+    }
+    if (target.tagName === 'A' || target.closest('a')) {
+      this.closeNavbar();
+    }
+  }
 
 }
---
+++ vasavi-new-website/src/app/thankyou-page/thankyou-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-thankyou-page',
+  imports: [RouterModule, RouterLink],
+  templateUrl: './thankyou-page.html',
+  styleUrl: './thankyou-page.css'
+})
+export class ThankyouPage {
+
+}
