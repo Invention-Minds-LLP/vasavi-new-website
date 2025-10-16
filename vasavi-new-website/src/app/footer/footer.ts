481f5e7c7aa8999113d9c0a5e9c9c4084bd267ed
nithithehero <mknithish03@gmail.com>
2025-10-16 11:37:12 +0530

    frontend changes

--- vasavi-new-website/src/app/footer/footer.ts
+++ vasavi-new-website/src/app/footer/footer.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, RouterModule],
+  imports: [RouterLink, RouterModule, CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class Footer {
+  constructor(public router: Router) { }
 
+   showDesigner(): boolean {
+    const inventionRoutes = ['/bariatric-surgery', '/cardiology', '/ent','/nephrology','/obstetrics-gynaecology','/oncology','/orthopedic','/pulmonology','/urology'];
+    return inventionRoutes.includes(this.router.url);
+  }
 }
--- vasavi-new-website/src/app/gallery/gallery.ts
+++ vasavi-new-website/src/app/gallery/gallery.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [RouterModule, RouterLink],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css'
 })
