63b1431500b16fea470bef4114217c7006226b3d
Rithish06 <rithish.manohar006@gmail.com>
2025-11-07 18:47:21 +0530

    recaptcha added and location changed

--- vasavi-new-website/src/environments/environment.ts
+++ vasavi-new-website/src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://vasavi-hospitals-812956739285.us-east4.run.app/api'
+    apiUrl: 'https://vasavi-hospitals-812956739285.us-east4.run.app/api',
+    // apiUrl: 'http://localhost:3000',
+    recaptchaSiteKey : '6LftOAUsAAAAABXY9i1GeVDEkfLO-D0b9slhpIy_'
   };
   
+
