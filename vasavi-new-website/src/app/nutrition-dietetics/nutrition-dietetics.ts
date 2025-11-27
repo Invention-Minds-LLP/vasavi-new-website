dbae41c53176c8b2d6da44b2c06a11b872c1a27c
Rithish06 <rithish.manohar006@gmail.com>
2025-11-27 21:03:12 +0530

    removed 2 more doctors and added dr Sunitha in website

--- vasavi-new-website/src/app/nutrition-dietetics/nutrition-dietetics.ts
+++ vasavi-new-website/src/app/nutrition-dietetics/nutrition-dietetics.ts
@@ -6,10 +6,11 @@ import { ContactFom } from "../contact-fom/contact-fom";
 import { SubNavbar } from "../sub-navbar/sub-navbar";
 import { Cta } from "../cta/cta";
 import { Meta, Title } from '@angular/platform-browser';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-nutrition-dietetics',
-  imports: [CarouselModule, CommonModule, ContactFom, SubNavbar, Cta],
+  imports: [CarouselModule, CommonModule, ContactFom, SubNavbar, Cta, RouterLink],
   templateUrl: './nutrition-dietetics.html',
   styleUrl: './nutrition-dietetics.css'
 })
